f1c6cb0e99424f07bffc8d86feea428ac592ccfa
Thilina-Pathirage <thilinapathirage@gmail.com>
2021-04-30 00:50:21 +0530

    init

---
+++ functions/api.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const serverless = require('serverless-http');
+var request = require("request");
+const app = express();
+const router = express.Router();
+
+router.get('/',(req,res) => {
+    request(
+        "https://www.fossnsbm.org",
+        function(error, response, body){
+            if(!error && response.statusCode == 200){
+                res.json({
+                    'servise-name':'Blog',
+                    'stats': 'Active'
+                });
+            }else{
+                res.json({
+                    'servise-name':'Blog',
+                    'stats': 'Inactive'
+                });
+            }
+        }
+    );
+});
+
+
+app.use('/', router)
+module.exports.handler = serverless(app);
